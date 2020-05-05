789e164555ec6f9b07ff51b72d12f0aefac97198
issaknife <53504667+issaknife@users.noreply.github.com>
2020-05-05 23:27:08 +0300

    multiple image upload with user uid

--- src/views/Imagetest.vue
+++ src/views/Imagetest.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="img">
-    <input type="file" @change="uploadFile($event.target.files)" accept="image/*" />
+    <input
+      type="file"
+      @change="uploadFile($event.target.files)"
+      accept="image/*"
+      multiple
+    />
     <br />
     <img width="200px" :src="source" />
   </div>
@@ -9,23 +14,26 @@
 <script>
 import firebase from "firebase/app";
 import "firebase/storage";
+import "firebase/auth";
 export default {
   data() {
     return {
-      source: ""
+      source: []
     };
   },
   methods: {
     async uploadFile(files) {
-      const file = files.item(0);
-      const metadata = {
-        contentType: "image/png"
-      };
-      const storageRef = firebase.storage().ref();
-      const imageRef = storageRef.child(`images/${files.name}`);
-      await imageRef.put(file, metadata);
-      const downloadURL = await imageRef.getDownloadURL();
-      this.source = downloadURL;
+      const uid = await firebase.auth().currentUser.uid;
+      for (let file of files) {
+        const metadata = {
+          contentType: "image/jpg"
+        };
+        const storageRef = firebase.storage().ref();
+        const imageRef = storageRef.child(`images/${uid}/${file.name}`);
+        await imageRef.put(file, metadata);
+        // const downloadURL = await imageRef.getDownloadURL();
+        // this.source = downloadURL;
+      }
     }
   }
 };
